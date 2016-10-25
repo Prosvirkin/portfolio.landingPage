3c0371ddfde039c0d62f26d87c566ed79da9b60c
xTw1st <lp.k.sl@mail.ru>
2016-10-25 23:52:28 +0600

    add
    add new project

---
+++ projects/Ink-tattoo/js/common.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+ $(".toggle-menu").click(function() {
+ 	$(this).next().slideToggle();
+ });
+
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox({
+		hideOnContentClick: false,
+		hideOnOverlayClick: true,
+	});
+
+
+	//Плавный скролл до блока .div по клику на .scroll
+	//Документация: https://github.com/flesler/jquery.scrollTo
+	$("a.scroll").click(function() {
+		$.scrollTo($(".div"), 800, {
+			offset: -90
+		});
+	});
+
+	
+});
